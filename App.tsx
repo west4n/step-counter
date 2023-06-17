bf10bb2a802e9fba1c838d7c72aa3c1f8c334332
Ivan Kurski <iskurskiii@gmail.com>
2023-06-17 17:36:49 +0300

    Render Text Statistics

--- App.tsx
+++ App.tsx
@@ -1,20 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+
+type StaticValueProps = {
+	label: string
+	value: string
+}
+
+const StaticValue = ({ label, value }: StaticValueProps) => (
+	<View style={styles.valueContainer}>
+		<Text style={styles.label}>{label}</Text>
+		<Text style={styles.value}>{value}</Text>
+	</View>
+)
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<Text>Hello world!</Text>
+
+			<View style={{ flexDirection: 'row' }}>
+				<StaticValue label='Шаги' value='1219' />
+				<StaticValue label='Дистанция' value={'3 км'} />
+			</View>
+
+			<StaticValue label='Пролетов пройдено' value='3' />
+
+			<StatusBar style='auto' />
+		</View>
+	)
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	container: {
+		flex: 1,
+		backgroundColor: '#000',
+		justifyContent: 'center',
+		padding: 12,
+	},
+	valueContainer: {
+		marginRight: 50,
+		marginVertical: 10,
+	},
+	label: {
+		fontSize: 20,
+		color: '#FFF',
+	},
+	value: {
+		fontSize: 35,
+		color: '#AFB3BE',
+		fontWeight: '400',
+	},
+})
